5a2ec1ea06899cb38129454df99fdea3afb58cf9
ortuman <ortuman@gmail.com>
2017-02-12 13:45:07 +0100

    Minor changes.

--- mercury.go
+++ mercury.go
@@ -1,5 +1,5 @@
 //
-//  main.go
+//  mercury.go
 //  mercuryx
 //
 //  Copyright (c) 2016 Miguel Ángel Ortuño. All rights reserved.
@@ -9,19 +9,24 @@ package main
 
 import (
 	"os"
+	"fmt"
+	"flag"
 	"github.com/ortuman/mercury/server"
 	"github.com/ortuman/mercury/config"
-	"github.com/ortuman/mercury/logger"
 )
 
-const configFile = "/etc/mercury/mercury.conf"
-
 func main() {
+
+	var configFile string
+	flag.StringVar(&configFile, "config-file", "/etc/mercury/mercury.conf", "configuration path file")
+	flag.Parse()
+
 	// load configuration
 	if _, err := os.Stat(configFile); err == nil {
 		config.Load(configFile)
 	} else {
-		logger.Warnf("main: couldn't load config file '%s': %v", configFile, err)
+		fmt.Fprintf(os.Stderr, "mercury: couldn't load config file '%s': %v", configFile, err)
+		os.Exit(-1)
 	}
 
 	srv := server.NewServer()
